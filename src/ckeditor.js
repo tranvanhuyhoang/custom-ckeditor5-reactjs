0c3c8f2fb0fed6d52a110a965f70417d9fe6c8ac
hoangtran <tranvanhuyhoang@gmail.com>
2021-04-14 09:23:40 +0700

    add plugin import image via url

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -11,6 +11,7 @@ import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapte
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
@@ -31,8 +32,47 @@ import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 
+//add plugin
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
+import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';	//IMPORT NEW PLUGIN
 import ImageInsert from '@ckeditor/ckeditor5-image/src/imageinsert';	//IMPORT NEW PLUGIN
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';	//IMPORT NEW PLUGIN
+import Font from '@ckeditor/ckeditor5-font/src/font';		//IMPORT NEW PLUGIN
+
+class InsertImage extends Plugin {
+	init() {
+			const editor = this.editor;
+
+			editor.ui.componentFactory.add( 'insertImage', locale => {
+					const view = new ButtonView( locale );
+
+					view.set( {
+							label: 'Insert image',
+							icon: imageIcon,
+							tooltip: true
+					} );
+
+					// Callback executed once the image is clicked.
+					view.on( 'execute', () => {
+							const imageUrl = prompt( 'Nhập vào URL ảnh' );
+
+							editor.model.change( writer => {
+									const imageElement = writer.createElement( 'image', {
+											src: imageUrl
+									} );
+
+									// Insert the image in the current selection location.
+									editor.model.insertContent( imageElement, editor.model.document.selection );
+							} );
+					} );
+
+					return view;
+			} );
+	}
+}
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -43,6 +83,7 @@ ClassicEditor.builtinPlugins = [
 	Autoformat,
 	Bold,
 	Italic,
+	Underline,
 	BlockQuote,
 	CKFinder,
 	CloudServices,
@@ -55,14 +96,17 @@ ClassicEditor.builtinPlugins = [
 	Indent,
 	Link,
 	List,
-	MediaEmbed,
+	// MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
-	Table,
-	TableToolbar,
+	// Table,
+	// TableToolbar,
 	TextTransformation,
 	Alignment,	//ADD PLUGIN
-        ImageInsert,	//ADD PLUGIN
+  ImageInsert,	//ADD PLUGIN
+	InsertImage,	//ADD PLUGIN
+	ImageResize, //ADD PLUGIN
+	Font, //ADD PLUGIN
 ];
 
 // Editor configuration.
@@ -74,27 +118,53 @@ ClassicEditor.defaultConfig = {
 			'alignment',	//DISPLAY HERE
 			'bold',
 			'italic',
-			'link',
-			'bulletedList',
-			'numberedList',
-			'|',
-			'outdent',
-			'indent',
+			'underline',
+			'fontSize', //DISPLAY HERE
+			'fontColor', //DISPLAY HERE
+			'fontBackgroundColor', //DISPLAY HERE
 			'|',
 			'insertImage',	//DISPLAY HERE
+			'bulletedList',
+			'numberedList',
+			// '|',
+			// 'outdent', //not use
+			// 'indent', //not use
+			'|',
+			'link',
 			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
+			// 'insertTable', //not use
+			// 'mediaEmbed', //not use
 			'undo',
 			'redo'
 		]
 	},
 	image: {
+		styles: [
+			'alignLeft', 'alignCenter', 'alignRight'
+		],
+		resizeOptions: [
+			{
+					name: 'resizeImage:original',
+					label: 'Original',
+					value: null
+			},
+			{
+					name: 'resizeImage:50',
+					label: '50%',
+					value: '50'
+			},
+			{
+					name: 'resizeImage:75',
+					label: '75%',
+					value: '75'
+			}
+		],
 		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
+			'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight',
 			'|',
-			'imageTextAlternative'
+			'resizeImage',
+			'|',
+			'imageTextAlternative',
 		]
 	},
 	table: {
@@ -102,6 +172,20 @@ ClassicEditor.defaultConfig = {
 			'tableColumn',
 			'tableRow',
 			'mergeTableCells'
+		]
+	},
+	fontSize: {
+		options: [
+			9,
+			11,
+			13,
+			'default',
+			17,
+			19,
+			21,
+			25,
+			27,
+			30
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
